f97b09416c1c0045a09a875ef2ec33d00bd8f19c
Yuvraj-choudhary <79357332+Yuvraj-choudhary@users.noreply.github.com>
2021-12-04 21:33:54 +0530

    main

---
+++ README.md
@@ -0,0 +1,33 @@
+This is a [Expo](https://expo.dev/) project bootstrapped with [`expo init`](https://docs.expo.dev/).
+
+## Getting Started
+
+First, install all the dependencies:
+
+```bash
+npm install
+# or
+yarn install
+```
+
+Then, run the development server:
+
+```bash
+npm start
+# or
+yarn start
+```
+
+Open [http://localhost:19002/](http://localhost:19002/) with your browser and click on Run on Android or IOS Emulator.
+
+You can start editing the page by modifying `/App.tsx`. The page auto-updates as you edit the file. 🚀🚀🚀
+
+## Learn More
+
+To learn more about Expo, take a look at the following resources:
+
+- [Expo Documentation](https://docs.expo.dev/get-started/installation/) - learn about Expo features and API.
+
+## Optional
+
+You can also install [BetterComments](https://marketplace.visualstudio.com/items?itemName=aaron-bond.better-comments) - to get better experience with coding.
---
+++ firebase.jsx
@@ -0,0 +1,31 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth, signInWithEmailAndPassword, createUserWithEmailAndPassword } from "firebase/auth";
+import { getStorage } from "firebase/storage";
+import { initializeFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyA9t2JBRTaSERAXU2m0_ze4XTO4nNRwmHM",
+  authDomain: "whatsapp-mobile-6032e.firebaseapp.com",
+  projectId: "whatsapp-mobile-6032e",
+  storageBucket: "whatsapp-mobile-6032e.appspot.com",
+  messagingSenderId: "334843456125",
+  appId: "1:334843456125:web:3526c42a13ff5968dbdf14",
+  measurementId: "G-WDR6F8CZ56",
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const storage = getStorage(app);
+export const db = initializeFirestore(app, {
+  experimentalForceLongPolling: true,
+});
+
+export function signIn(email, password) {
+  return signInWithEmailAndPassword(auth, email, password);
+}
+
+export function signUp(email, password) {
+  return createUserWithEmailAndPassword(auth, email, password);
+}
